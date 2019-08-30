501ead0a739896308faa3a1814cd4b48a488a66f
Jhannet Jancko <jhannet.jancko.camacho@gmail.com>
2019-08-29 21:08:41 -0400

    fase 2

--- appSample/my-app/src/notas/Main.js
+++ appSample/my-app/src/notas/Main.js
@@ -9,35 +9,44 @@ import {notas} from '../notas'
       )
   });*/
 
-const listaDeCards = notas.map((todo, i) => {
-    return (
-    <div className="col-md-4" key={i}>
-        <div className="card mt-4">
-        <div className="card-title text-center">
-            <h3>{todo.titulo}</h3>
-            <span className="badge badge-pill badge-danger ml-2">
-            {todo.prioridad}
-            </span>
-        </div>
-        <div className="card-body">
-        </div>
-        <div className="card-footer">
-            <button className="btn btn-danger">
-            Delete
-            </button>
-        </div>
-        </div>
-    </div>
-    )
-});
-
 class Main extends React.Component {
     constructor(context, props){
         super(context, props)
         console.log(notas);
+        this.state = {
+            notas
+        }
     }
     
+    eliminarItem(index) {
+        console.log(index,typeof index);
+        this.setState({
+          notas: this.state.notas.filter(item=>item.id !== index)
+        })
+    }
+
     render() {
+        const listaDeCards = this.state.notas.map((todo, i) => {
+            return (
+            <div className="col-md-4" key={i}>
+                <div className="card mt-4">
+                <div className="card-title text-center">
+                    <h3>{todo.titulo}</h3>
+                    <span className="badge badge-pill badge-danger ml-2">
+                    {todo.prioridad}
+                    </span>
+                </div>
+                <div className="card-body">
+                </div>
+                <div className="card-footer">
+                    <button className="btn btn-danger" onClick={this.eliminarItem.bind(this,todo.id)}>
+                        Delete {todo.id}
+                    </button>
+                </div>
+                </div>
+            </div>
+            )
+        });
         return (
             <div className="App">
                <h1>Note App</h1> 
